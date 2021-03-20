01db16d2d0590d46ff90177b80141308a33fda81
SatoShun0302 <bengalcat302@gmail.com>
2021-03-20 22:27:23 +0900

    mainをGetXに適した形に修正/メインページクラス作成

---
+++ lib/components/main_feature/main_screen.dart
@@ -0,0 +1,9 @@
+import 'package:battle_operation2_app/importer/dart_importer.dart';
+
+/// アプリ起動後最初に表示される画面
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
